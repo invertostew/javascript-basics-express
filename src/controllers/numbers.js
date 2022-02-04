1d564ddf593f5d8501a75172f082cbf29392ef0d
Stuart Green <github.47a8z@aleeas.com>
2022-02-04 21:49:20 +0000

    subtract pass

--- src/controllers/numbers.js
+++ src/controllers/numbers.js
@@ -1,9 +1,14 @@
-const { add } = require('../lib/numbers');
+const { add, subtract } = require('../lib/numbers');
 
 const addNumbers = (req, res) => {
   res.status(200).json({ result: add(req.num1, req.num2) });
 };
 
+const subtractNumbers = (req, res) => {
+  res.status(200).json({ result: subtract(req.num2, req.num1) });
+};
+
 module.exports = {
   addNumbers,
+  subtractNumbers,
 };
--- src/routes/numbers.js
+++ src/routes/numbers.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const validateNumbers = require('../middleware/numbers');
-const { addNumbers } = require('../controllers/numbers');
+const { addNumbers, subtractNumbers } = require('../controllers/numbers');
 
 const numbersRouter = express.Router();
 
 numbersRouter.get('/add/:num1/and/:num2', validateNumbers, addNumbers);
+numbersRouter.get('/subtract/:num1/from/:num2', validateNumbers, subtractNumbers);
 
 module.exports = numbersRouter;
